ea7933669b1c4cdf35580dd38ba3c65d4eef97bc
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-11-24 22:04:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
 client.on('message', message => {
     if (message.content == "^fast") {
         var x = ["DreamKing",
@@ -77,7 +76,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Dream | ${client.guilds.size} ${client.users.size} servers/user`,"http://twitch.tv/YouTube")
+client.user.setGame(`^fast | ${client.guilds.size} ${client.users.size} servers/user`,"http://twitch.tv/YouTube")
 client.user.setStatus("dnd")
 });
 
