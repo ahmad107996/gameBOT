bec8de8a14381026a4746689eb944ce0bcd886a6
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-11-25 14:07:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`^fast | ${client.guilds.size} ${client.users.size} servers/user`,"http://twitch.tv/YouTube")
+client.user.setGame(`^fast | ${client.guilds.size} / ${client.users.size} servers/user`,"http://twitch.tv/YouTube")
 client.user.setStatus("dnd")
 });
 
