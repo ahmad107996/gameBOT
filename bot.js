2500b70eda0a498a5a0fdecdf0e5fd2ea9a22e75
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-12-01 23:21:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require('fs');
+
 var prefix = "^"; 
 client.on('message', message => {
     if (message.content == "^fast") {
